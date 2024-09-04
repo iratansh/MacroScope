f706c9f2a78efa1599f7c242e2ddbf6316c0dfc8
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-09-03 18:13:52 -0600

    Update views.py

--- Backend/forecasts/views.py
+++ Backend/forecasts/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from keras.models import load_model
 from .features import load_data
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import logging
 
 class PredictionView(APIView):
     def get(self, request, indicator):
@@ -9,5 +12,27 @@ class PredictionView(APIView):
         model = load_model(f'{indicator.lower()}_forecasting_model.h5')
         data = data[-10:].reshape((1, 10, 1))  
         prediction = model.predict(data)
-        return Response({'prediction': prediction[0][0]})
+        return JsonResponse({'prediction': prediction[0][0]})
+
+
+@api_view(['POST'])
+def gdp_data(request):
+    logging.info('GDP data requested')
+    # test restapi with some made up data
+    data = {
+        "labels": ["2018", "2019", "2020", "2021", "2022"],
+        "values": [1.8, 1.9, -2.1, 5.0, 3.2],
+        "label": "GDP Growth Rate (%)"
+    }
+    return JsonResponse(data)
+
+@api_view(['POST'])
+def unemployment_data(request):
+    # test restapi with some made up data
+    data = {
+        "labels": ["2018", "2019", "2020", "2021", "2022"],
+        "values": [5.8, 5.7, 9.5, 7.5, 6.9],
+        "label": "Unemployment Rate (%)"
+    }
+    return JsonResponse(data)
 
