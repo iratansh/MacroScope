c823a82f45d938cf6f6287c99cb1c8f88814dcc8
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-10-14 18:39:16 -0600

    add more urls

--- Backend/authentication/urls.py
+++ Backend/authentication/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from .views import register, activate
-from .views import login_user
+from .views import (
+    register, activate, logout_user, login_user, send_password_reset_email, reset_password, update_security, get_user_details
+)
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login_user'),
     path('activate/<str:uidb64>/<str:token>/', activate, name='activate'),
+    path('logout/', logout_user, name='logout_user'),
+    path('forgot-password/', send_password_reset_email, name='send_password_reset_email'),
+    path('update-security/', update_security, name='update_security'),
+    path('account-info/', get_user_details, name='account-info'),
+    path('reset-password/<str:uidb64>/<str:token>/', reset_password, name='reset_password'),
 ]
 
 
